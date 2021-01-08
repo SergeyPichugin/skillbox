d288bd9fc75025125da1c9639225ab5398c8a2a4
Serega <serega@MacBook-Pro-123.local>
2021-01-09 00:58:32 +0300

    fix class object, add transport

--- 7-oop-js/script.js
+++ 7-oop-js/script.js
@@ -16,7 +16,7 @@ class Transport{
         this.countPilot = countPilot
     }
 
-    run(speed){
+    run(speed = this.power){
         this.speed = speed
         console.log(`Скорость ${this.name} передвижения ${speed} км/час`)
     }
@@ -25,12 +25,19 @@ class Transport{
 
 // (2)
 class Auto extends Transport{
-    constructor(name, model, power, color, movement, door, places, countPilot){
-        super(name, model, power, color, movement, door, places, countPilot)        
+    constructor(name, model, power, color, movement, door, places, countPilot, transmission){
+        super(name, model, power, color, movement, door, places, countPilot)   
+        this.transmission = transmission
     }    
 }
 // (2)
 class Plane extends Transport{
+    constructor(name, model, power, color, movement, door, places, countPilot){
+        super(name, model, power, color, movement, door, places, countPilot)        
+    }
+}
+// (2)
+class Ship extends Transport{
     constructor(name, model, power, color, movement, door, places, countPilot){
         super(name, model, power, color, movement, door, places, countPilot)        
     }
@@ -43,9 +50,10 @@ class Train extends Transport{
 }
 
 // (3)
-let audi = new Auto('Audi', 'A3', 130, '#FF0000', 'road', 5, 4, 1)
+let audi = new Auto('Audi', 'A3', 130, '#FF0000', 'Дорожный транспорт', 5, 4, 1, 'Автоматическая')
+let lada = new Auto('Lada', 'Priora', 120, '#000', 'Дорожный транспорт', 3, 4, 1, 'Механическая')
 
-let airobus = new Plane('Airobus', 'A100', 700, '#FFF', 'fly', 1, 100, 2)
+let airobus = new Plane('Airobus', 'A100', 700, '#FFF', 'Летный транспорт', 1, 100, 2)
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -54,6 +62,7 @@ document.addEventListener('DOMContentLoaded', () => {
     btnForm.addEventListener('click', () => {
         
         console.table(audi)
+        console.table(lada)
         console.table(airobus)
     })
 
